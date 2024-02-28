42fd398cc1b161bedbcc467be54b7bc54156425e
cfhaak <c.f.haak@posteo.de>
2024-02-28 11:07:33 +0100

    new wf, taking data from transkribus-export, overwriting with working-data

--- shellscripts/fetch_editions.sh
+++ shellscripts/fetch_editions.sh
@@ -2,16 +2,27 @@
 # # def vars
 temp_path="./tmp_tmp/"
 zip_file="${temp_path}main.zip"
-fetched_editions_path="${temp_path}bv-working-data-main/data/editions/"
+fetched_transkribus_export_editions_path="${temp_path}bv-transkribus-export-main/editions/"
+fetched_working_data_path="${temp_path}bv-working-data-main/data/editions/"
 target_editions_path="./data/editions/"
-# # prepare folder structure
-rm -rf $temp_path
-mkdir $temp_path
-# # fetch data from repo, unzip, and move to destination
-wget https://github.com/bundesverfassung-oesterreich/bv-working-data/archive/refs/heads/main.zip -O $zip_file
-unzip $zip_file -d $temp_path
-# # check for target dir
-if [ ! -d "$target_editions_path" ]; then mkdir -p "$target_editions_path"; fi
-find $fetched_editions_path -name "*.xml" -exec cp "{}" "$target_editions_path" \;
-# # remove temp folder
+bv_transkribus_export_url="https://github.com/bundesverfassung-oesterreich/bv-transkribus-export/archive/refs/heads/main.zip"
+bv_working_data_url="https://github.com/bundesverfassung-oesterreich/bv-working-data/archive/refs/heads/main.zip"
+
+function fetch_data_and_copy() {
+    url=$1
+    editions_folder=$2
+    temp_path="./tmp_tmp/"
+    dump_path="${temp_path}main.zip"
+    target_editions_path="./data/editions/"
+    rm -rf $temp_path
+    mkdir $temp_path
+    if [ ! -d "$target_editions_path" ]; then mkdir -p "$target_editions_path"; fi
+    wget $url -O $dump_path
+    unzip $dump_path -d $temp_path
+    find $editions_folder -name "*.xml" -exec cp "{}" "$target_editions_path" \;
+}
+# get frech copy of transkribus_export
+fetch_data_and_copy $bv_transkribus_export_url $fetched_transkribus_export_editions_path
+# overwrite with data from working_data
+fetch_data_and_copy $bv_working_data_url $fetched_working_data_path
 rm -rf $temp_path
